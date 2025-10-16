102acc663fc8d293d58a03284c83d98858802e59
BRADDTELES <braddteles@gmail.com>
2025-10-16 16:31:37 -0300

    Como os códigos são estruturados

--- JavaScript/teste.js
+++ JavaScript/teste.js
@@ -1,2 +1,17 @@
-var nome = "Maria"
-console.log(nome)
+//instrução
+console.log("quebre 3 ovos")
+console.log("misture com a farinha")
+console.log("leve ao forno")
+
+//Bloco de códigos
+{
+  console.log("coma 1 pedaço de pizza")
+}
+
+{
+  console.log("teste")
+  {
+    console.log("teste")
+    console.log("teste")
+  }
+}
