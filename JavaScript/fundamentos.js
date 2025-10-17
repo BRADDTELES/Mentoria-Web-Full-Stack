6c4e85e0178926b9c4d715093035340b7bc90b05
BRADDTELES <braddteles@gmail.com>
2025-10-16 21:11:14 -0300

    Variáveis e seus tipos

--- JavaScript/fundamentos.js
+++ JavaScript/fundamentos.js
@@ -28,17 +28,24 @@ linhas
 /* console.log("comentário de multiplas linhas") */
 
 /* Variáveis e Constantes */
-let altura = 1.90
-let peso = 70
+/* let altura = 1.90
+let peso = 70 */
 
 //let resultado = peso / (altura * altura)
-const pi = 3.14
+//const pi = 3.14
 //console.log(pi)
-let resultado = pi * 2
-console.log(resultado)
+/* let resultado = pi * 2
+console.log(resultado) */
 
 /* 
 1) não iniciadas com números, apenas letras ou _
 2) Não utilizar caracteres especiais como: ç ` ~
 3) Não pode ser iguais as palavras reservadas
 */
+
+/* Variáveis e seus tipos */
+let nome = "Jamilton" //String
+let numero = 1200 //Number: inteiro ou decimais
+let decimal = 1200.50 //Number: decimal
+let negativo = -1200
+let avancarProximoEpisodio = false //Boolean: booleanos true ou false
