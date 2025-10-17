f91ee1b39bf6485762fd7414a2f906c4c74e4fb6
BRADDTELES <braddteles@gmail.com>
2025-10-16 21:03:23 -0300

    O que são variáveis e constantes? (Prática)

---
+++ JavaScript/fundamentos.js
@@ -0,0 +1,44 @@
+//instrução
+/* console.log("quebre 3 ovos")
+console.log("misture com a farinha")
+console.log("leve ao forno") */
+
+//Bloco de códigos
+/* {
+  console.log("coma 1 pedaço de pizza")
+}
+
+{
+  console.log("teste")
+  {
+    console.log("teste")
+    console.log("teste")
+  }
+} */
+
+//Comentário
+//console.log("comentário 1 linha")
+
+/*
+Comentário 
+de 
+multiplas 
+linhas 
+*/
+/* console.log("comentário de multiplas linhas") */
+
+/* Variáveis e Constantes */
+let altura = 1.90
+let peso = 70
+
+//let resultado = peso / (altura * altura)
+const pi = 3.14
+//console.log(pi)
+let resultado = pi * 2
+console.log(resultado)
+
+/* 
+1) não iniciadas com números, apenas letras ou _
+2) Não utilizar caracteres especiais como: ç ` ~
+3) Não pode ser iguais as palavras reservadas
+*/
