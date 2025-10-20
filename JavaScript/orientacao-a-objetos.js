e2a3dbed494235157265f538c245c45bc1262046
BRADDTELES <braddteles@gmail.com>
2025-10-20 15:09:42 -0300

    Pilares da orientação a Objetos - Encapsulamento 2

--- JavaScript/orientacao-a-objetos.js
+++ JavaScript/orientacao-a-objetos.js
@@ -185,3 +185,51 @@ console.log(usuario.calcularDesconto( "FESTA10" )) */
 
 // const carro = new Carro()
 // carro.frear()
+
+// Encapsulamento, modificadores de acesso e getters e setters
+class ContaBancaria {
+    constructor(){
+        // private, protected e public
+        this._numeroConta = 0
+        this._saldo = 0
+    }
+
+    sacar( valorSaque ){
+        this._saldo = this._saldo - valorSaque
+    }
+
+    depositar( valorDeposito ){
+        this._saldo += valorDeposito
+    }
+
+    get saldo(){
+        return this._saldo
+    }
+
+    set saldo( novoSaldo ){
+        if ( novoSaldo  > 0 ){
+            this._saldo = novoSaldo
+        }
+    }
+    get numeroConta(){
+        return "Número: " + this._numeroConta
+    }
+
+    set numeroConta( numero ){
+        if ( numero > 0 ){
+            this._numeroConta = numero
+        }
+    }
+
+}
+
+const conta = new ContaBancaria()
+//conta.numeroConta = 60
+//conta.numeroConta = 60
+//console.log( conta.numeroConta )
+
+conta.saldo = 500 // segura
+conta.sacar(50)
+conta.depositar(100)
+
+console.log( conta.saldo )
