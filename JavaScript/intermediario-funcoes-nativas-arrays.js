6990b3cf3b52e95db6760cf94ec16615e17c7ad7
BRADDTELES <braddteles@gmail.com>
2025-10-21 10:47:29 -0300

    Parâmetros e retornos de função

---
+++ JavaScript/intermediario-funcoes-nativas-arrays.js
@@ -0,0 +1,30 @@
+//Parâmetros padrão
+//NaN -> Not a Number (não é um número)
+/* function somar(numero1, numero2){
+    numero1 = isNaN(numero1) ? 0 : numero1
+    numero2 = isNaN(numero2) ? 0 : numero2
+    return numero1 + numero2
+}
+console.log(somar(2, "jamilton"))
+console.log(somar(2)) */
+
+//Valor padrão do es2025
+// function somar( numero1 = 0, numero2 = 0){
+//     return numero1 + numero2
+// }
+
+// console.log(somar(2, 10))
+
+//Parâmetros e retornos opcionais
+function calcularSalario(salario, desconto){
+    //desconto = isNaN(desconto) ? 0 : desconto
+    if ( salario == 0 ){
+        console.log("Salário não informado")
+        return 0
+    } else {
+        return salario - desconto
+    }
+}
+
+let resultado = calcularSalario(0, 20)
+console.log(resultado)
