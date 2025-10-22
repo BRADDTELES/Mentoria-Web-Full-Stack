d86582d7882a2d7cec6bc5eff9dabe2ca7f3100a
BRADDTELES <braddteles@gmail.com>
2025-10-22 16:07:12 -0300

    Array: ForEach

--- JavaScript/intermediario-funcoes-nativas-arrays.js
+++ JavaScript/intermediario-funcoes-nativas-arrays.js
@@ -189,3 +189,39 @@ let arrayTexto = texto.split(",")
 
 console.log( arrayTexto ) */
 
+
+/* --- Array: ForEach --- */
+// const lista = ["Jamilton", 'Ana', "Marcela", "Pedro"]
+
+// // for(indice in lista){
+// //     console.log(lista[indice])
+// // }
+
+// /* const percorrer = function(item, i, arr){
+//     console.log(item)
+// }
+
+// lista.forEach(percorrer) */
+
+// /* lista.forEach( function(item, i, arr){
+//     console.log(item)
+//     }) */
+    
+// // lista.forEach( item => console.log(item) )
+
+// Array.prototype.percorrer = function(funcao){
+//     // console.log(this)
+//     for(let i = 0; i < this.length; i++){
+//         // console.log("teste")
+//         funcao(this[i], i, this)
+//     }
+// }
+
+// const funcao = function(item, i, arr){
+//     console.log(item)
+// }
+
+// lista.percorrer(funcao)
+
+
+
