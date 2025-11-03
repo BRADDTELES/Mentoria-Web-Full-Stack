131a2adece6affc8deabec56580cba35f49bfa59
BRADDTELES <braddteles@gmail.com>
2025-11-03 06:11:34 -0300

    Type Annotation

---
+++ Typescript/src/modules/basic-types/type-annotation.ts
@@ -0,0 +1,9 @@
+// let product: string = 'Micro-ondas'; // Type Annotation, usa a anotação ": string"
+let product = 'Micro-ondas'; // Type Inference, sem type annotation, sem inserir o ": string"
+let price = 647.52
+// usamos type annotation dentro do constructor da função
+export function display(product: string, price: number){ 
+    console.log(product.toUpperCase(), price.toFixed(0));
+}
+
+display(product, price)
