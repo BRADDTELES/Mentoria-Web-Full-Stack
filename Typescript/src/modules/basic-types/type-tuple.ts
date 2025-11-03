d8b8a803bba3e1161cedf7b3b1ebcefae930d77e
BRADDTELES <braddteles@gmail.com>
2025-11-03 07:01:51 -0300

    Tipo Tuple

---
+++ Typescript/src/modules/basic-types/type-tuple.ts
@@ -0,0 +1,9 @@
+let filmArray: (number | string | boolean)[] = [1, 'Guerra Civil', true]
+let filmTuple: [number, string, boolean] = [2, 'Um Lugar Silencioso: Dia Um', false]
+let filmTupleOpcionalPosition: [number, string, boolean?, string?] = [2, 'Um Lugar Silencioso: Dia Um']
+
+const [idArr, titleArr, availableArr] = filmArray
+const [id, title, available] = filmTuple
+const [idOpcional, titleOpcional, availableOpcional] = filmTupleOpcionalPosition
+
+console.log(id);
