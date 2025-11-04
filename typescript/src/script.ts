3938cbd6550804f7e2881cc9ac0b40e5ffe1e2c5
BRADDTELES <braddteles@gmail.com>
2025-11-04 10:51:20 -0300

    Executando os scripts compilados no Browser
    Recaptulando:
    - o Browser (navegador) não é suportado pelo o CommonJS, mas sim pelo o ES Modules
    - o CommonJS é sim suportado pelo o NodeJS
    
    Mudanças:
    - precisa alterar no arquivo tsconfig.json o "module" para "ES2022" ou seja o ES Module

--- typescript/src/script.ts
+++ typescript/src/script.ts
@@ -1,6 +1,9 @@
-export function sum(a: number, b: number) {
+/* export function sum(a: number, b: number) {
   console.log('Dentro do escopo do módulo');
   return {
     result: a + b,
   };
+} */
+export function bootstrap() {
+  console.log('Script carregado com sucesso');
 }
