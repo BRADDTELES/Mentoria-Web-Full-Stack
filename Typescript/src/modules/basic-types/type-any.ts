b9dcda8f7bee52f6a78ca86fb2619380713019e4
BRADDTELES <braddteles@gmail.com>
2025-11-04 05:59:29 -0300

    Tipo Any - deve ser evitado ao máximo, não é recomendado

---
+++ Typescript/src/modules/basic-types/type-any.ts
@@ -0,0 +1,14 @@
+/* any é um tipo que aceita qualquer coisa.
+    Deve ser evitado ao máximo, não é recomendado usar.
+    O ideal é sempre tipar os dados e evitar o uso do any.
+*/
+export function handleFileUpload(file: any) {
+    console.log(`Nome: ${file.name}`);
+    console.log(`Tamanho: ${file.zise}`);
+}
+
+const file = { 
+    name: 'lista_de_funcionarios.txt',
+    size: 456123789
+}
+handleFileUpload(file)
