b818995b856197caa6862ac8b387f0c9b29a7fa2
BRADDTELES <braddteles@gmail.com>
2025-11-07 15:48:42 -0300

    Non-null Assertion (!)

---
+++ typescript/dist/modules/narrowing/non-null-assertion.js
@@ -0,0 +1,18 @@
+export const bootstrap = () => {
+    /* Non-Null Assertion
+    esse operador requer Cuidados!
+    ele diz ao TS que o elemento não é nulo, mas isso pode ser um erro de programação.
+    */
+    const subtitle = document.getElementById('subtitle');
+    subtitle.style.color = 'green';
+    /* if (subtitle) {
+      subtitle.style.color = 'green';
+    } */
+    const getProducts = () => {
+        // return ['Smartphone', 'Headset']
+        return null; // Lançar o Error no Browser
+    };
+    // define que a atribuição será de um valor null ou undefined
+    const products = getProducts(); // Error: em tempo de execução no Browser
+    products.map(item => console.log(item));
+};
