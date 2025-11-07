34282fe82eebdf299d6b0bc9283f7780766d7981
BRADDTELES <braddteles@gmail.com>
2025-11-07 17:44:35 -0300

    Verificando o Tipo do Objeto (Instanceof) Parte 2

--- typescript/dist/modules/narrowing/instaceof.js
+++ typescript/dist/modules/narrowing/instaceof.js
@@ -11,33 +11,44 @@ export const bootstrap = () => {
             return this.holder;
         }
     }
-    class CheckingAccount {
+    class CheckingAccount extends BankAccount {
         overdraftLimit;
-        constructor(overdraftLimit) {
+        constructor(holder, balance, overdraftLimit) {
+            super(holder, balance);
             this.overdraftLimit = overdraftLimit;
         }
         getOverdraftLimit() {
             return this.overdraftLimit;
         }
     }
-    const a = new BankAccount('Jorge', 15000);
-    const b = new CheckingAccount(25000);
-    console.log('Jorge', 15000);
-    console.log(a);
-    console.log(typeof a);
-    console.log(a instanceof BankAccount);
-    console.log(a instanceof CheckingAccount);
-    function showDetails(account) {
-        if (account instanceof BankAccount) {
-            console.log(account.getHolder());
+    class SavingsAccount extends BankAccount {
+        interestRate;
+        constructor(holder, balance, interestRate) {
+            super(holder, balance);
+            this.interestRate = interestRate;
         }
-        else if (account instanceof CheckingAccount) {
-            console.log(account.getOverdraftLimit());
-        }
-        else {
-            console.error('Conta não identificada');
+        getInterestRate() {
+            return this.interestRate;
         }
     }
-    showDetails(a);
-    showDetails(b);
+    // lista de contas bancárias
+    const accountList = [
+        new CheckingAccount('Alice', 1500, 300),
+        new SavingsAccount('João', 4000, 0.005),
+        new CheckingAccount('Maria', 10000, 3000),
+        new SavingsAccount('Paulo', 1000, 0.005),
+    ];
+    function processAccounts(accounts) {
+        accounts.forEach(account => {
+            // console.log(account)
+            if (account instanceof CheckingAccount) {
+                console.log('Processando a conta corrente: ', account.getOverdraftLimit());
+            }
+            else if (account instanceof SavingsAccount) {
+                console.log('Processando a conta poupanca: ', account.getInterestRate());
+            }
+            console.log('--------------------');
+        });
+    }
+    processAccounts(accountList);
 };
