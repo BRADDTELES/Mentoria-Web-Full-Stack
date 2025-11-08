d25760fda831f92cf10b7c9d46b3acad97a68bf7
BRADDTELES <braddteles@gmail.com>
2025-11-07 21:48:54 -0300

    Array.isArray

---
+++ typescript/src/modules/narrowing/array-isarray.ts
@@ -0,0 +1,15 @@
+export const bootstrap = (): void => {
+  /* Array isArray */
+    let arr: number[] | undefined = [1, 2, 3];
+//   let arr: number[] | undefined;
+
+  console.log('Typeof de array: ', typeof arr);
+  console.log('Instanceof de array: ', arr instanceof Array);
+  console.log('Array.isArray(): ', Array.isArray(arr));
+
+  if (Array.isArray(arr)) {
+    arr.map((item) => {
+      console.log(item);
+    });
+  }
+};
