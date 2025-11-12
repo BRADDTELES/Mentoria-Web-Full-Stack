c9eca0b34ca757726369fe045c92ad8febf2e6b7
BRADDTELES <braddteles@gmail.com>
2025-11-12 06:02:18 -0300

    Introdução a Interfaces

---
+++ typescript/src/modules/interface/interface.ts
@@ -0,0 +1,25 @@
+export const bootstrap = (): void => {
+  /* Interface */
+  interface Resume {
+    fullName: string;
+    email: string;
+    skills: Skill[];
+    addSkill?: (skill: Skill) => boolean
+  };
+
+  interface Skill {
+    name: string;
+    level: 'beginner' | 'intermediate' | 'advanced';
+  };
+
+  const MyResume: Resume = {
+    fullName: 'Jorge Sant Ana',
+    email: 'jorge@argus-academy.com',
+    skills: [
+      { name: 'JavaScript', level: 'advanced' },
+      { name: 'TypeScript', level: 'advanced' },
+    ],
+  };
+
+  console.log(MyResume);
+};
